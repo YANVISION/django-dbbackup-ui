8827b513e67b2f3acd3422e75d2f7fa37b7069c4
Abdelaziz CHIBOUB <aziz.chiboub@gmail.com>
2022-11-22 12:57:37 +0100

    Update urls.py

--- dbbackup_ui/urls.py
+++ dbbackup_ui/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.urls import re_path
 from .views import BackupView
 
 urlpatterns = [
-    url(r'^backup-database-and-media/$', BackupView.as_view(), name="backup_view"),
+    re_path(r'^backup-database-and-media/$', BackupView.as_view(), name="backup_view"),
 ]
